07c55a57ff358d89816ad5d47706de54722bc1cf
Giuseppe Vacanti <g.vacanti@cosine.eu>
2026-07-04 10:21:15 +0200

    Fix .ods loading: use pandas engine id "odf", not "odfpy"
    pd.read_excel(engine=...) expects pandas's registered engine name
    ("odf"), not the pip package name ("odfpy"). Every .ods load raised
    ValueError: Unknown engine.

--- curvelab/data_manager.py
+++ curvelab/data_manager.py
@@ -36,7 +36,7 @@ class DataManager:
             ".tsv": lambda p: pd.read_csv(p, sep="\t"),
             ".xlsx": lambda p: pd.read_excel(p),
             ".xls": lambda p: pd.read_excel(p),
-            ".ods": lambda p: pd.read_excel(p, engine="odfpy"),
+            ".ods": lambda p: pd.read_excel(p, engine="odf"),
             ".json": lambda p: pd.read_json(p),
             ".parquet": lambda p: pd.read_parquet(p),
             ".h5": lambda p: pd.read_hdf(p),
